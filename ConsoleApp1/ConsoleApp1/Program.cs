4f67ef661013839d672e853189d2b574804e7297
saul benitez silva <saulbs26@hotmail.com>
2019-08-24 19:08:22 -0500

    Ciclos

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, y, d;
+            Console.WriteLine("Imprime números del x hasta y");
+            Console.WriteLine("Cuanto vale x?");
+            n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Cuanto vale y?");
+            y = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("De cuanto en cuanto va a hacer?");
+            d = Convert.ToInt32(Console.ReadLine());
+            if (n > y)
+            {
+                for (int i = n; i > y; i -= d)
+                {
+                    Console.WriteLine("i = " + i);
+                }
+            }
+            else
+            {
+                for (int i = n; i < y; i += d)
+                {
+                    Console.WriteLine("i = " + i);
+                }
+            }
+        }
+    }
+}
